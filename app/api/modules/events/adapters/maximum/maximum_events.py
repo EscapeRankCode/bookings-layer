975d16b96dcae462771fca2b27b9b54af9659a81
xoselolo <xose.lopez@students.salle.url.edu>
2022-07-26 21:53:46 +0200

    EventTicketsResponse object creation fails 3

--- app/api/modules/events/adapters/maximum/maximum_events.py
+++ app/api/modules/events/adapters/maximum/maximum_events.py
@@ -69,6 +69,19 @@ class MaximumApiEvents(ApiEventsInterface):
         print("-- Response first ticket type:")
         print(str(response.tickets_groups[0].tickets[0].ticket_type))
 
+        print("-- Response first ticket info . min_option:")
+        print(str(response.tickets_groups[0].tickets[0].ticket_info.min_option))
+        print("-- Response first ticket info . max_option:")
+        print(str(response.tickets_groups[0].tickets[0].ticket_info.max_option))
+        print("-- Response first ticket info . default:")
+        print(str(response.tickets_groups[0].tickets[0].ticket_info.default))
+        print("-- Response first ticket info . single_unit_value:")
+        print(str(response.tickets_groups[0].tickets[0].ticket_info.single_unit_value))
+        print("-- Response first ticket info . price_per_unit:")
+        print(str(response.tickets_groups[0].tickets[0].ticket_info.price_per_unit))
+        print("-- Response first ticket info . currency:")
+        print(response.tickets_groups[0].tickets[0].ticket_info.min_option)
+
         return response
 
     def encapsulate_event_tickets(self, event, event_tickets_request: EventTicketsRequest) -> EventTicketsResponse:
