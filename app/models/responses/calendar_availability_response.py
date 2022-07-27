5efe13685dfdcfa2e97b5b5a2173f384b95c4315
xoselolo <xose.lopez@students.salle.url.edu>
2022-07-27 10:29:12 +0200

    __iter__, __str__ and __repr__ methods in calendar_availability_response.py

--- app/models/responses/calendar_availability_response.py
+++ app/models/responses/calendar_availability_response.py
@@ -8,6 +8,19 @@ class Event:
         self.event_id = event_id
         self.availability = availability
 
+    def __iter__(self):
+        yield from {
+            "time": self.time,
+            "event_id": self.event_id,
+            "availability": self.availability
+        }.items()
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Day:
     def __init__(self, year: int, month: int, day: int, day_availability: int, events):
@@ -17,11 +30,38 @@ class Day:
         self.day_availability = day_availability
         self.events = events
 
+    def __iter__(self):
+        yield from {
+            "year": self.year,
+            "month": self.month,
+            "day": self.day,
+            "day_availability": self.day_availability,
+            "events": self.events
+        }.items()
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Calendar:
     def __init__(self, timezone, days):
         self.timezone = timezone
         self.days = days
+
+    def __iter__(self):
+        yield from{
+            "timezone": self.timezone,
+            "days": self.days
+        }
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class CalendarAvailabilityResponse:
@@ -29,6 +69,19 @@ class CalendarAvailabilityResponse:
         self.booking_system_id = booking_system_id
         self.bs_config_id = bs_config_id
         self.calendar = calendar
+
+    def __iter__(self):
+        yield from{
+            "booking_system_id": self.booking_system_id,
+            "bs_config_id": self.bs_config_id,
+            "calendar": self.calendar
+        }
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class CalendarAvailabilityResponseEncoder(JSONEncoder):
