c40012a4d6bfbc364218e984e546a3dca06381f4
xoselolo <xose.lopez@students.salle.url.edu>
2022-08-13 18:32:40 +0200

    Maximum with its events

--- app/api/modules/events/adapters/maximum/maximum_events.py
+++ app/api/modules/events/adapters/maximum/maximum_events.py
@@ -71,14 +71,14 @@ class MaximumApiEvents(ApiEventsInterface):
             for price in prices:
 
                 # TODO: HARDCODED TICKET INFO
-                # ticket_info = TicketInfoOption(False, 1, float(prices[price]), "€")  # Good one (counter)
+                ticket_info = TicketInfoOption(False, 1, float(prices[price]), "€")  # Good one (counter)
                 # ticket_info = TicketInfoCounter(0, 4, 0, 1, 1, "€")  # Counter
-                ticket_info = TicketInfoCheck(False, 1, 1, "€")  # Check
+                # ticket_info = TicketInfoCheck(False, 1, 1, "€")  # Check
 
                 # TODO HARDCODED TICKET INFO
-                # ticket = Ticket(price + apis_strings.BS_MAXIMUM_TICKET_PEOPLE, TicketType.option, ticket_info)  # Good one (counter)
+                ticket = Ticket(price + apis_strings.BS_MAXIMUM_TICKET_PEOPLE, TicketType.option, ticket_info)  # Good one (counter)
                 # ticket = Ticket(price + apis_strings.BS_MAXIMUM_TICKET_PEOPLE, TicketType.counter, ticket_info)  # Counter
-                ticket = Ticket(price + apis_strings.BS_MAXIMUM_TICKET_PEOPLE, TicketType.check, ticket_info)  # Check
+                # ticket = Ticket(price + apis_strings.BS_MAXIMUM_TICKET_PEOPLE, TicketType.check, ticket_info)  # Check
 
                 tickets.append(ticket)
 
