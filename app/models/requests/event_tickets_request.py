c90e8d62736338256c775015642d1d53670fd1aa
xoselolo <xose.lopez@students.salle.url.edu>
2022-08-15 20:49:42 +0200

    Maximum Form : Todo Test - some minimum changes

--- app/models/requests/event_tickets_request.py
+++ app/models/requests/event_tickets_request.py
@@ -2,7 +2,7 @@
 class EventTicketsRequest:
     def __init__(self, request_data):
         self.event_date = request_data['event_date']  # dd/mm/yyyy
-        self.event_time = request_data['event_time']  # dd/mm/yyyy
+        self.event_time = request_data['event_time']  # HH:MM
         self.booking_system_id = request_data['booking_system_id']  # int
         self.bs_config = request_data['bs_config']  # object
         self.event_id = request_data['event_id']  # string
