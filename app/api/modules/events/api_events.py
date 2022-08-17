badaf6ffc05f85c319a666b351490f6c3928e50b
xoselolo <xose.lopez@students.salle.url.edu>
2022-08-17 10:58:23 +0200

    event_form maximum done!

--- app/api/modules/events/api_events.py
+++ app/api/modules/events/api_events.py
@@ -39,8 +39,6 @@ class ApiEvents(metaclass=ApiEventsMeta):
         # Depending on the booking system
         if event_form_request.booking_system_id == general_utils.BS_ID_MAXIMUM:
             form = self.maximum_api_events.get_event_form(event_form_request)
-            print("FORM TO JSON")
-            print(str(form.to_json()))
             return form.to_json()
             #  return json.dumps(form.to_json())
 
