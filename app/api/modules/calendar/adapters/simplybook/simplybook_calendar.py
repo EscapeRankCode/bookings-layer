32df79a15ae95c65f92f7add8c05530ecf94ba3a
xoselolo <xose.lopez@students.salle.url.edu>
2022-08-27 14:04:18 +0200

    url change from external booking system simplybook (get slots), param now is date=, param before was date_to=

--- app/api/modules/calendar/adapters/simplybook/simplybook_calendar.py
+++ app/api/modules/calendar/adapters/simplybook/simplybook_calendar.py
@@ -69,7 +69,7 @@ class SimplybookApiCalendar(ApiCalendarInterface):
 
             # Call to get all slots
             url = general_utils.SIMPLYBOOK_BS_HOST + general_utils.SIMPLYBOOK_BS_get_slots + \
-                  "?date_to=" + date_x.strftime(general_utils.SIMPLYBOOK_DATE_FORMAT) + \
+                  "?date=" + date_x.strftime(general_utils.SIMPLYBOOK_DATE_FORMAT) + \
                   "&provider_id=" + str(api_request.bs_config['provider_id']) + \
                   "&service_id=" + str(service)
             payload = {}
